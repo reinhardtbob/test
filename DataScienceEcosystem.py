e9daf1fbed9383e0a2343711190565d6041c37a9
reinhardtbob <140835619+reinhardtbob@users.noreply.github.com>
2023-07-28 14:06:11 -0500

    upload jupyter notebook

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#  Exercise 2: Create a markdown cell with title "Data Science Tools and Ecosystem
+# 
+# # Data Science Tools and Ecosystem
+
+#  Exercise3 Create a markdown cell for introduction
+#  
+# ## In this notebook, Data Science Tools and Ecosystem are summarized
+
+# Exercise 10 Insert a markdown cell to list objectives
+# 
+# **Objectives:**
+# 
+# * List popular languages for Data Science
+# * Libraries for Data Science
+# * Open source tools for Data Science
+# * Application Programing Interfaces
+# 
+
+# Exercise 4 Create a markdown cell to list data science languages
+# 
+# ## Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. Scala
+# 4. Julia
+# 5. Leaflet
+
+#  Exercise 5 Create a markdown cell to list data science libraries
+# 
+# ### Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. ggplot
+# 2. Pandas
+# 3. NumPy
+# 4. SciPy
+# 5. Matplotlib 
+
+#  Exercise 6 Create a markdown cell with a table to Data Science Tools
+# 
+# |Data Science Tools|
+# |--|
+# |Jupyter Notebooks|
+# |Rstudio|
+# |Visual Studio VS Code|
+# 
+# 
+
+#  Exercise 7 -  Create a markdown cell introducing arithmetic expression examples
+# 
+# ### Below are a few examples of evaluating arithmetic expression in Python.
+# 
+
+#  Exercise 8 - Create a code cell to multiply and add numbers
+
+#  This is a simple arithmetic expression to multiply and then add integers
+
+# In[1]:
+
+
+(3*4)+5
+
+
+#  Exercise 9 - Create a code cell to convert minutes to hours
+
+# This will convert 200 minutes to hours by dividing by 60
+
+# In[2]:
+
+
+200/60
+
+
+#  Exercise 10 - Insert a markdown cell to list objectives
+
+# Exercise 11 - Create a markdown cell to indicate the Author's name
+# ## Author
+# Robert Reinhardt
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
